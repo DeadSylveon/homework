43a0d4952d40e0f8bf433203bafb4649ccf47693
rigad <dadedidad@mail.ru>
2021-02-02 02:32:06 +0200

    homework
    lesson-1

--- src/Fruit.java
+++ src/Fruit.java
@@ -1,3 +1,3 @@
-public class Fruit {
+public abstract class Fruit {
 
 }
--- src/Main.java
+++ src/Main.java
@@ -25,10 +25,52 @@ public class Main {
         System.out.println(Arrays.toString(strings1));
         ArrayList<String> strings2 = getArrayList(strings1);
         System.out.println("After using the method it is already an ArrayList:");
-        System.out.println(strings2.toString());
+        System.out.println(strings2.toString() + "\n");
 
-        Fruit[] apples = {new Orange(), new Apple(), new Apple()};
-        System.out.println(new Box<>(new ArrayList<>(Arrays.asList(apples))).getWeight());
+        System.out.println("Big task #3:" );
+        ArrayList<Fruit> oranges = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            oranges.add(new Orange());
+        }
+
+        ArrayList<Fruit> apples = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            apples.add(new Apple());
+        }
+
+        ArrayList<Fruit> otherApples = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            otherApples.add(new Apple());
+        }
+
+        try {
+            Box<Fruit> OrangesBox = new Box<>(oranges);
+            System.out.println("Weight of box with oranges:" + OrangesBox.getWeight());
+
+            Box<Fruit> ApplesBox = new Box<>(apples);
+            System.out.println("Weight of box with apples:" + ApplesBox.getWeight());
+
+            System.out.println("Are two boxes equal? -" + OrangesBox.compare(ApplesBox) + "\n");
+
+            System.out.println("Remove one apple");
+            ApplesBox.removeFruit(0);
+            System.out.println("Weight of box with oranges:" + OrangesBox.getWeight());
+            System.out.println("Weight of box with apples:" + ApplesBox.getWeight());
+            System.out.println("Are two boxes equal now? -" + OrangesBox.compare(ApplesBox) + "\n");
+
+            Box<Fruit> AnotherOrangesBox = new Box<>(otherApples);
+            System.out.println("Weight of box with apples:" + ApplesBox.getWeight());
+            System.out.println("Weight of box with other apples:" + AnotherOrangesBox.getWeight());
+            System.out.println("Moving apples from ApplesBox to AnotherApplesBox...");
+            ApplesBox.moveFruits(AnotherOrangesBox);
+            System.out.println("Weight of box with apples:" + ApplesBox.getWeight());
+            System.out.println("Weight of box with other apples:" + AnotherOrangesBox.getWeight());
+        }
+        catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
+
     }
 
     public static <T> void swapElements(T[] array, int firstElemIndex, int secondElemIndex){
